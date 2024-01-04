215634265ceaaff7008a3b459abf19de31005baf
burzza <bineshburzaa@gmail.com>
2024-01-04 15:57:55 +0545

    like lottie

--- lib/features/home/screens/home_page.dart
+++ lib/features/home/screens/home_page.dart
@@ -1,12 +1,16 @@
+import 'dart:developer';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kavyanepal/config/config.dart';
 import 'package:kavyanepal/features/home/models/quotes_model_meta.dart';
 import 'package:kavyanepal/features/home/providers/quote_api_provider.dart';
 import 'package:kavyanepal/features/home/widgets/widgets.dart';
+import 'package:lottie/lottie.dart';
 import 'package:screenshot/screenshot.dart';
 
 import '../../more/dbquery/favorite_db_query.dart';
@@ -70,11 +74,26 @@ class _QuotesWidgetState extends ConsumerState<QuotesWidget> {
   Widget build(BuildContext context) {
     final favDbQuery = ref.watch(faveDbProvider);
 
+    final showAnimation = useState<bool>(false);
+
+    startAnimation() {
+      if (!showAnimation.value) {
+        showAnimation.value = true;
+
+        Future.delayed(const Duration(seconds: 2), () {
+          showAnimation.value = false;
+        });
+      }
+    }
+
     return GestureDetector(
       onDoubleTap: () {
-        setState(() {
-          favDbQuery.toggleFav(model: widget.quotes);
-        });
+        if (favDbQuery.getId(model: widget.quotes) == null) {
+          startAnimation();
+          setState(() {
+            favDbQuery.toggleFav(model: widget.quotes);
+          });
+        }
       },
       child: Stack(
         alignment: Alignment.center,
@@ -113,6 +132,18 @@ class _QuotesWidgetState extends ConsumerState<QuotesWidget> {
                     ),
                   ),
                 ).animate().fade().scale(),
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 0,
+                  right: 0,
+                  child: Visibility(
+                    visible: showAnimation.value,
+                    child: LottieBuilder.asset(
+                      'assets/animations/like.json',
+                    ),
+                  ),
+                )
               ],
             ),
           ),
